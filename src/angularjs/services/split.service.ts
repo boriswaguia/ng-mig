1eb14b5e70bb6def9806d1d718bfa5e1c06c26ca
Boris Waguia <bwa@adorsys.de>
2020-02-09 12:12:11 +0100

    Handle extraction of all angular function declarations

--- src/angularjs/services/split.service.ts
+++ src/angularjs/services/split.service.ts
@@ -4,8 +4,62 @@ import traverse from '@babel/traverse';
 import template from '@babel/template';
 import generate from '@babel/generator';
 import { Node } from '@babel/types';
+import { File } from '@babel/types';
+
 import * as fs from 'fs';
 
+function extractContentToFile(file: File, sourceTemplate: any, contentId: string) {
+  traverse(file, {
+    FunctionDeclaration: function(xPath) {
+      // jsonPrint('controller', xPath.node);
+      if(xPath.isFunctionDeclaration() && xPath.node.loc && xPath.node.id && xPath.node.id.name === contentId) {
+
+        const code = generate((xPath.node as Node)).code
+
+        const result = sourceTemplate({
+          content: code,
+          contentId
+        }) as Statement[];
+
+        const p = program(result);
+        const controllerContent = generate(p).code;
+        fs.writeFileSync(`${contentId.toLowerCase()}.component.ts`, controllerContent);
+      }
+    }
+  })
+}
+
+function extract(expression: CallExpression, file: File, fileTemplate: string) {
+
+  const sourceTemplate = template(fileTemplate, {
+    allowImportExportEverywhere: true
+  });
+
+  if (expression.arguments && expression.arguments.length > 0) {
+    if(expression.callee.type == "MemberExpression") {
+      const callee: MemberExpression = expression.callee;
+      const callerName = callee.property['name']; // can be .config(..) .controller('xx',..), .constanst
+      const argument = expression.arguments.length === 1 ? expression.arguments[0] : expression.arguments[1];
+
+      let functionId = ""
+      if(argument.type === "StringLiteral") {
+        functionId = argument.value;
+      } else if(argument.type === "Identifier") {
+        functionId = argument.name;
+      }
+      console.log(`${callerName} - ${functionId}`);
+
+      extractContentToFile(file, sourceTemplate, functionId);
+      if (callee.object.type === "CallExpression") {
+        extract(callee.object, file, fileTemplate);
+      }
+    }
+  } else {
+    if(expression.callee.type === "MemberExpression" && expression.callee.object.type === "CallExpression") {
+      extract(expression.callee.object, file, fileTemplate);
+    }
+  }
+}
 const splitDeclaration = (traverseResult: TraverseResult): void => {
 
   const {file, modulePath} = traverseResult
@@ -23,49 +77,11 @@ const splitDeclaration = (traverseResult: TraverseResult): void => {
 
   `;
 
-  const sourceTemplate = template(fileTemplate, {
-    allowImportExportEverywhere: true
-  });
-
   if (node.expression.type === "CallExpression") {
     const expression: CallExpression = node.expression;
-    if (expression.arguments && expression.arguments.length > 0) {
-      if(expression.callee.type == "MemberExpression") {
-        const callee: MemberExpression = expression.callee;
-        const callerName = callee.property['name'];
-
-        if(callerName === "controller") {
-          const argument = expression.arguments.length === 1 ? expression.arguments[0] : expression.arguments[1];
-          let controllerName: string = "";
-          if(argument.type === "StringLiteral") {
-            controllerName = argument.value;
-          } else if(argument.type === "Identifier") {
-            controllerName = argument.name;
-          }
-          console.log('controllerName', controllerName);
-          traverse(file, {
-            FunctionDeclaration: function(xPath) {
-              // jsonPrint('controller', xPath.node);
-              if(xPath.isFunctionDeclaration() && xPath.node.loc && xPath.node.id && xPath.node.id.name === controllerName) {
-
-                const code = generate((xPath.node as Node)).code
-
-                const result = sourceTemplate({
-                  content: code,
-                  contentId: controllerName
-                }) as Statement[];
-
-                const p = program(result);
-                const controllerContent = generate(p).code;
-                fs.writeFileSync(`${controllerName.toLowerCase()}.component.ts`, controllerContent);
-                console.log(controllerContent);
-              }
-            }
-          })
-        }
-      }
-    }
+    extract(expression, file, fileTemplate);
   }
 };
+
 
 export {splitDeclaration};
