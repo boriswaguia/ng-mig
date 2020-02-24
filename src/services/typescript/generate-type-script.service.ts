ba6e4615b4298ed3acde952af524b47b5d59d26c
Boris Waguia <bwa@adorsys.de>
2020-02-24 18:21:50 +0100

    Add support recursive folder parsing

--- src/services/typescript/generate-type-script.service.ts
+++ src/services/typescript/generate-type-script.service.ts
@@ -1,18 +1,51 @@
 import { extractAllClassMetaInfos } from './search/extract-class-meta.service';
+import { extractDeclaredIds } from './search/declared-identifiers.service';
+import { createTsClass } from './convert/create-ts-class.service';
+import { fixMissingThisKeys } from './convert/fix-missing-this-keys.service';
+import traverse from '@babel/traverse';
+import * as t from '@babel/types';
+import { parseSourceTypeModule } from '../../vendors/helpers/code-parser.helper';
+import { FilePath, FolderPath } from '../split/module.type';
+import { openFile, writeFileSync } from '../../vendors/helpers/file.helper';
+import { getSourceFiles } from '../../vendors/helpers/dirwalk.helper';
+import geneate from '@babel/generator';
 
-const parseToTypescript = (source: string) => {
-  // 1.
-  const classMeta = extractAllClassMetaInfos(source);
-  // 2.
-  // convert constructor params to private typescript contructor params
-  // convert assigments variables into private properties
-  // create class
-  // create class properties (convert init)
-  // create constructor
-  // add params
-  // init property
-  // add class methods
-  // return code
+const replaceCurrentClass = (source: string, currentClass: t.ClassDeclaration): t.File => {
+  const file: t.File = parseSourceTypeModule(source);
+
+  traverse(file, {
+    ClassDeclaration: function(xPath) {
+      xPath.replaceWith(currentClass);
+    }
+  });
+  return file;
+}
+
+const parseToTypescript = (source: string): t.File => {
+  const availableVariables = extractDeclaredIds(source);
+  const classDeclaration = createTsClass(extractAllClassMetaInfos(source));
+  const newClass = fixMissingThisKeys(classDeclaration, availableVariables);
+
+  const newSource: t.File = replaceCurrentClass(source, newClass);
+  return newSource;
 };
 
-export { parseToTypescript };
+const parseToTypescriptFile = (filePath: FilePath): [string, t.File] => {
+  const parsed = parseToTypescript(openFile(filePath));
+  return [filePath, parsed];
+};
+const parseToTypescriptFiles = (filePaths: FilePath[]) => filePaths.map(filePath => {
+  console.log(`started parsing ${filePath}`);
+  const result = parseToTypescriptFile(openFile(filePath));
+  return result;
+});
+const parseToTypescriptFolder = (folderPath: FolderPath) => {
+  const files = getSourceFiles(folderPath);
+  const parsedFiles = parseToTypescriptFiles(files);
+  parsedFiles.forEach(file => {
+    writeFileSync(file[0], geneate(file[1]).code)
+    console.log(`writting file to disk : ${file[0]}`);
+  })
+}
+
+export { parseToTypescript, parseToTypescriptFolder };
